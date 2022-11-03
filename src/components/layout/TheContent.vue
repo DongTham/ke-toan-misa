8c81a72288384e948026a6bb96eb5df1c4723c57
DongTham <quocdongxh10@gmail.com>
2022-11-03 08:37:00 +0700

    feat: Thêm định dạng ngày giờ

--- src/components/layout/TheContent.vue
+++ src/components/layout/TheContent.vue
@@ -63,7 +63,7 @@
                                     <td>{{item.EmployeeCode}}</td>
                                     <td>{{item.EmployeeName}}</td>
                                     <td>{{item.Gender == 0 ? 'Nam': item.Gender == 1 ? 'Nữ' : item.Gender == 2 ? 'Khác' : item.Gender }}</td>
-                                    <td>{{item.DateOfBirth}}</td>
+                                    <td>{{date(item.DateOfBirth)}}</td>
                                     <td>{{item.IdentityNumber}}</td>
                                     <td>{{item.IdentityPlace}}</td>
                                     <td>{{item.DepartmentName}}</td>
@@ -73,8 +73,8 @@
                                     <td class="td-context-menu">
                                         <button class="btn-edit">Sửa</button>
                                         <div class="select-contextmenu">
-                                            <the-button class="btn-selectdown combobox__button" @click="handleSelectContextMenu"></the-button>
-                                            <div class="combobox__data" v-show="showSelectContextMenu">
+                                            <the-button class="btn-selectdown combobox__button" @click="handleSelectContextMenu(index)"></the-button>
+                                            <div class="combobox__data" v-show="showSelectContextMenu == index ? true : false">
                                                 <div class="data-item">Nhân bản</div>
                                                 <div class="data-item">Xóa</div>
                                                 <div class="data-item">Ngừng sử dụng</div>
@@ -96,24 +96,26 @@
                                 <input type="text" class="input-main combobox__input select__record-input" v-bind:value="pageSize + ' bản ghi trên 1 trang'" readonly>
                                 <button class="btn-selectdown combobox__button" @click="handleShowPageSize"></button>
                                 <div class="combobox__data" v-show="showSelectPageSize" >
-                                    <div class="data-item" v-for="(item, index) in selectPageSize" :key="index" @click="handleSelectPageSize(item.Value)">{{item.Value}} bản ghi trên 1 trang</div>
+                                    <div class="data-item" v-for="(item, index) in selectPageSize" :key="index" @click="handleSelectPageSize(item.Value)" v-bind:class="item.Value == pageSize ? 'active-item' : ''">{{item.Value}} bản ghi trên 1 trang</div>
                                 </div>
                             </div>
-
-                            <button class="btn btn-prev" v-if="pageNumber == 1 ? disableButtonPrev : !disableButtonPrev" v-bind:disabled="disableButtonPrev">Trước</button>
-                            <div class="btn btn-pagination"></div>
-                            <button class="btn btn-next" v-if="pageNumber == totalPages ? disableButtonPrev : !disableButtonPrev" v-bind:disabled="disableButtonPrev">Sau</button>
+                            
+                            <button class="btn btn-prev" v-bind:disabled="pageNumber == 1 ? disableButtonPrev : !disableButtonPrev" @click="handleChangePage(pageNumber - 1)">Trước</button>
+                            <div class="pagination">
+                                <div class="btn-pagination" v-for="(item, index) in totalPages" :key="index" @click="handleChangePage(item)" v-bind:class="item == pageNumber ? 'active-item' : ''">{{item}}</div>
+                            </div>
+                            
+                            <button class="btn btn-next" v-bind:disabled="pageNumber == totalPages ? !disableButtonNext : disableButtonNext" @click="handleChangePage(pageNumber + 1)">Sau</button>
                         </div>
                     </div>
                 </div>
             </div>
-
-            div 
 </template>
   
 <script>
 import TheButton from '../base/TheButton.vue'
 import {selectPageSize} from '../../i18n/i18nCommon'
+import moment from 'moment';
 
 export default {
     name:"TheContent",
@@ -142,14 +144,19 @@ export default {
                 me.totalPages = data.TotalPage;
             })
         },
+
+        date(date) {
+            return date ? moment(date).format('DD/MM/YYYY') : date;
+        },
+
         handleShowPageSize(){
             const me = this;
             me.showSelectPageSize = me.showSelectPageSize ? false : true;
         },
         
-        handleSelectContextMenu(){
+        handleSelectContextMenu(index){
             const me = this;
-            me.showSelectContextMenu = me.showSelectContextMenu ? false : true;
+            me.showSelectContextMenu = index;
         },
 
         handleSelectPageSize(el){
@@ -157,19 +164,26 @@ export default {
             me.showSelectPageSize = false;
             me.pageSize = el;
             me.getAllData();
+        },
+
+        handleChangePage(el){
+            const me = this;
+            me.pageNumber = el;
+            me.getAllData();
         }
     },
     data(){
         return{
             showSelectPageSize: false, 
-            showSelectContextMenu: false,
+            showSelectContextMenu: -1,
             employeeList: [],
             pageSize: 20,
             pageNumber: 1,
             numberRecords: 0,
             totalPages: 0,
             selectPageSize: selectPageSize,
-            disableButtonPrev: true
+            disableButtonPrev: true,
+            disableButtonNext: false
         }
     }
 }
